c2a9a99c94cffb18e49601981625616ac0214348
Diego Daniel Jofré Winterstetter <diegojofre7489@gmail.com>
2022-09-20 18:19:28 -0300

    Se agregan operadores ternarios

--- index.js
+++ index.js
@@ -1,74 +1,133 @@
-class Usuario{
-    constructor(pin, contraseña){
+class Usuario {
+    constructor(pin, contrasena) {
         this.pin = pin
-        this.contraseña = contraseña
+        this.contrasena = contrasena
 
     }
 }
 
 let usuarios = []
-usuarios.push(new Usuario(7444,"aaa"))
 
-const datos = document.querySelector("#datos")
+let submitRegister = document.querySelector("#submitRegister")
+submitRegister.addEventListener("click", registrarUsuario)
 
+function registrarUsuario(event) {
 
-function registrarUsuario(){
+    sessionStorage.removeItem(0 )
+    event.preventDefault();
+    console.log(event);
 
-    usuarios.push(new Usuario(7424,"ava"))
+    let formRegister = document.querySelector("#register")
+    console.dir(formRegister)
 
-    let pinx = datos.pin.value;
-    let pins = datos.pin2.value
-    let contraseñax = datos.contraseña.value 
-    let contraseñas = datos.contraseña2.value
-    let a=false
-    let b=false
-    
-    
-    
-    do {
-
-    if (pinx==pins) {
-
-        a=true
-
-    }else{
-        
-        a=true
-        alert("Los valores no coinciden. Intente nuevamente")
-        
-    }   
-        
-    } while (a!=true);
+    let pinx = formRegister[0].value
+    let pins = formRegister[1].value
+    let contraseñax = formRegister[2].value
+    let contraseñas = formRegister[3].value
+    let a = false
+    let b = false
 
     do {
+
+        (pinx == pins) ? a=true : alert("Los valores no coinciden. Intente nuevamente"), a = true
+
+    } while (a != true);
+
+    do {
+
+        (contraseñax == contraseñas) ? b = true : alert("Los valores no coinciden. Intente nuevamente2"), b = true
+
+    } while (b != true);
+
+    console.log(pinx)
+    console.log(pins)
+    console.log(contraseñas)
+    console.log(contraseñax)
+
+    if (pinx == pins && contraseñax == contraseñas) {
+
+        usuarios.push(new Usuario(pinx, contraseñas))
+
+    }
+
+    const token = generarToken(pinx, contraseñax)
+    sessionStorage.setItem("token", token)
+
+    //Prueba fallida de querer ocultar contenido. No ingresa al if. 
+
+    const autenticado = existToken()
+    if (autenticado && pinx == pins && contraseñax == contraseñas) {
+        console.log("entre")
+        formRegister.className = "oculto"
+        let visibleh2 = document.querySelector("#registerh2")
+        visibleh2.className = "oculto"
+    }
+}
+
+function generarToken(pin, contrasena) {
+    return pin + contrasena
+}
+
+function existToken() {
+
+    if (sessionStorage.getItem("token") !== null) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+
+
+let submitLogin = document.querySelector("#submitLogin")
+submitLogin.addEventListener("click", login)
+function login(event) {
+
+    event.preventDefault();
+    console.log(event);
+
+    let formLogin = document.querySelector("#login")
+    console.dir(formLogin)
+
+    let pine = formLogin[0].value
+    let password = formLogin[1].value
+    let pinDB = " "
+    let passwordDB = " "
+
+    console.log(usuarios)
+
+    if (usuarios.indexOf(pine) && usuarios.indexOf(password)) {
+        console.log(usuarios)
+        pinDB = pine
+        passwordDB = password
+
+        console.log(pinDB)
+        console.log(passwordDB)
+    }
     
-        if (contraseñax==contraseñas) {
-    
-            b=true
-    
-        }else{
-            
-            b=true
-            alert("Los valores no coinciden. Intente nuevamente2")
-            
-        }
+    if (usuarios.includes(pinDB) || usuarios.includes(passwordDB)) alert("Credenciales incorrectas")
+    if (pinDB.trim() && passwordDB.trim()) alert("¡Se ha logeado con éxito!")
+    else alert("Credenciales incorrectas")
 
     
-        } while (b!=true);
-        
-        usuarios.push(new Usuario(pinx,contraseñax))
-        
+
 
 }
 
-function mostrarUsuarios(){
+let ver = document.querySelector("#ver")
+ver.addEventListener("click", mostrarUsuarios)
+function mostrarUsuarios(event) {
 
-    usuarios.forEach((a) =>{
+    event.preventDefault()
+
+    usuarios.forEach((a) => {
+
 
         console.log(a)
     })
 
-    
+
 
 }
 
